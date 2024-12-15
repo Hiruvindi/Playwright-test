5141b55d12f768348f88df8a6bbba683c3f9ad69
Hiruvindi <hiruvindi2000@gmail.com>
2024-12-15 21:56:34 +0530

    Added 2 test case

--- tests/example2.spec.ts
+++ tests/example2.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("Wiley test 1 - Login", async ({ page }) => {
+test("Wiley test 2 - Register", async ({ page }) => {
   // Test script not work properly due to cloudfare human verifications :(
 
   // Go to the web page
@@ -12,10 +12,10 @@ test("Wiley test 1 - Login", async ({ page }) => {
   await page.locator("#indivLogin").click({ delay: 1000 });
 
   // Click on first <ul> <li> <a> tag which contains the induvidual login
-  await page.locator("ul li a").first().click({ delay: 1000 });
+  await page.locator("ul li a .btn").first().click({ delay: 1000 });
 
   // Check if the URL is correct
-  await expect(page).toHaveURL("https://wiley.scienceconnect.io/login");
+  await expect(page).toHaveURL("https://wiley.scienceconnect.io/register");
 
   // Fill the email input field
   await page.locator("input[name='email']").fill("hiruvindi2000@gmail.com");
@@ -23,8 +23,14 @@ test("Wiley test 1 - Login", async ({ page }) => {
   // Press enter key
   await page.keyboard.press("Enter");
 
+  // Fill the OTP input field
+  await page.locator("input[id='otp-input-0']").fill("123456");
+
+  // Press enter key
+  await page.keyboard.press("Enter");
+
   // Fill the password input field
-  await page.locator("input[name='password']").fill("1qaz2wsx#@A");
+  await page.locator("input[name='password']").fill("1qaz2wsx#@");
 
   // Press enter key
   await page.keyboard.press("Enter");
